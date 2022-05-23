ca86a3f2d37a30b5560ad4eea3621e1f3c77777f
hi Jacker <hijacker@hideMacBook-Pro.local>
2022-05-23 09:15:10 +0800

    my new nvim configuration v0.0.1

---
+++ lua/lsp/cmp.lua
@@ -0,0 +1,52 @@
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- For `vsnip` users.
+      vim.fn["vsnip#anonymous"](args.body)
+
+      -- For `luasnip` users.
+      -- require('luasnip').lsp_expand(args.body)
+
+      -- For `ultisnips` users.
+      -- vim.fn["UltiSnips#Anon"](args.body)
+
+      -- For `snippy` users.
+      -- require'snippy'.expand_snippet(args.body)
+    end,
+  },
+  formatting = require('lsp.ui').formatting,
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    -- For vsnip users.
+    { name = "vsnip" },
+
+    -- For luasnip users.
+    -- { name = 'luasnip' },
+
+    --For ultisnips users.
+    -- { name = 'ultisnips' },
+
+    -- -- For snippy users.
+    -- { name = 'snippy' },
+  }, { { name = "buffer" }, { name = "path" } }),
+
+  mapping = require("keybindings").cmp(cmp),
+})
+
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+})
